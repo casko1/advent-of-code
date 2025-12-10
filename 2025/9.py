85323a8aba37c0e3c712960f2b7ea206871d5e47
casko1 <casermanrok@gmail.com>
2025-12-10 04:07:59 +0100

    Performance improvement

--- 2025/9.py
+++ 2025/9.py
@@ -11,11 +11,10 @@ def area(x1, y1, x2, y2):
 points = [(int(a), int(b)) for line in lines for a, b in re.findall(r"(\d+),(\d+)", line)]
 edges = [(points[i], points[(i + 1) % len(points)]) for i in range(len(points))]
 squares = [((x1, y1), (x2, y2), area(x1, y1, x2, y2)) for (x1, y1), (x2, y2) in combinations(points, 2)]
+squares.sort(key=lambda t: t[2], reverse=True)
 
-max_area = max(squares, key=lambda t: t[2])
-print(max_area[2])
+print(squares[0][2])
 
-max_area = 0
 for (x1, y1), (x2, y2), a in squares:
     if all(
             min(ey1, ey2) >= max(y1, y2)
@@ -24,6 +23,5 @@ for (x1, y1), (x2, y2), a in squares:
             or max(ex1, ex2) <= min(x1, x2)
             for (ex1, ey1), (ex2, ey2) in edges
     ):
-        max_area = max(max_area, a)
-
-print(max_area)
+        print(a)
+        break
